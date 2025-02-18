e2d3ea5fed799c87edefb085b5a6834ebc9d52d8
shrutia15 <91371968+shrutia15@users.noreply.github.com>
2025-02-18 13:16:25 +0530

    Frontend src uploaded

---
+++ frontend/src/AdminPages/price.jsx
@@ -0,0 +1,15 @@
+
+import AdminNavbar from '../components/NavBars/AdminNavbar';
+
+import PricingPage from '../Pricing/pricing';
+const AdminCalPrice = () => {
+  
+  return (
+    <div>
+      <AdminNavbar/>
+      <PricingPage/>
+    </div>
+  );
+};
+
+export default AdminCalPrice;
---
+++ frontend/src/CustomerPages/tracking.jsx
@@ -0,0 +1,16 @@
+// import React, { useState } from 'react';
+import CustomerNavbar from '../components/NavBars/customerNavbar';
+import TrackingPage from '../Tracking/track';
+const CustomerTrackParcle = () => {
+  
+
+  return (
+    <div>
+        <CustomerNavbar/>
+        <TrackingPage/>
+    </div>
+   
+  );
+};
+
+export default CustomerTrackParcle;
