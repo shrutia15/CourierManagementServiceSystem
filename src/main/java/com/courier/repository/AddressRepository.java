64108d2977efce9cf8b683b0d7c7c71b2e76fa74
shrutia15 <91371968+shrutia15@users.noreply.github.com>
2025-02-18 13:16:54 +0530

    Delete src directory

--- src/main/java/com/courier/repository/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.courier.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.courier.pojos.Address;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-
-}
